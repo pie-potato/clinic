d962f8b7f4a34cc62784ecb3875f710cd10d595e
babys <ikartushin12@gmail.com>
2023-02-27 16:09:43 +0500

    final?

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import video from 'https://disk.yandex.ru/i/KTJXD2wuugIMjA';
 
 export default function Description() {
     return <>
@@ -10,10 +9,7 @@ export default function Description() {
             <p className="mini-p">В своей практике мы используем передовые методы  диагностики и лечения заболеваний</p>
             <button className="description-button">ЗАПИСАТЬСЯ</button>
         </div>
-        <video width="439"  height="440" controls className="video">
-            
-            <source src="../img/pxp.mp4" type="video/mp4" />
-        </video>
+        <iframe className="video" width="440" height="440" src="https://www.youtube.com/embed/EK2zrhecUTw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     </>;
 }
